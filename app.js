37fae37c64f94377e5f7a0fbc4027e7f38d99513
Marine Bianchi <marine.bianchi@gmail.com>
2023-11-25 23:06:03 +0100

    card scroll horizontal ok manque l'effet de retournement

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     // Code JavaScript ici
     console.log('Le DOM est chargé.');
   });
+
+  $(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true
+  });
+  $(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+      $(".custom-carousel .item").not($(this)).removeClass("active");
+      $(this).toggleClass("active");
+    });
+  });
