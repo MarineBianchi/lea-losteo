053a59a29ec4735a7e6c204b79de2dd99ee266ea
Marine Bianchi <marine.bianchi@gmail.com>
2023-11-25 11:28:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Code JavaScript ici
+    console.log('Le DOM est chargé.');
+  });
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+
+
+module.exports = {
+  content: [
+    './index.html',
+  ],
+  theme: {
+    theme: {
+      extend: {
+        colors: {
+          transparent: 'transparent',
+          'beige': '#F7F1E4',
+          'marron': '#E3BBA0',
+          'blanc': '#ffffff',
+          'jaune': '#FDC77D',
+        },
+        screens: {
+          sm: '480px',
+          md: '768px',
+          lg: '976px',
+          xl: '1440px',
+        },
+        fontFamily: {
+          sans: ['DM sans', 'sans-serif'],
+        },
+        fontSize: {
+          sm: ['14px', '20px'],
+          base: ['16px', '24px'],
+          lg: ['20px', '28px'],
+          xl: ['24px', '32px'],
+        }
+      },
+    },
+    plugins: [],
+    safelist: ['bg-beige', 'bg-marron', 'bg-jaune'],
+    
+
+  },
+}
+
