323e6654e179ff8344b31a24dd83c0c772e20015
Marine Bianchi <marine.bianchi@gmail.com>
2023-12-12 23:22:53 +0100

    modifs front top

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 
+function toggleMenu() {
+  var menu = document.getElementById('mobile-menu');
+  var openIcon = document.getElementById('menu-open-icon');
+  var closeIcon = document.getElementById('menu-close-icon');
+
+  if (menu.style.display === 'block') {
+      menu.style.display = 'none';
+      openIcon.style.display = 'block';
+      closeIcon.style.display = 'none';
+  } else {
+      menu.style.display = 'block';
+      openIcon.style.display = 'none';
+      closeIcon.style.display = 'block';
+  }
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
+
     const cards = document.querySelectorAll('.card');
   
     cards.forEach(card => {
@@ -11,8 +29,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const picturesContainer = document.querySelector('.picturesContainer');
 
-    const scrollLeftButton = document.getElementById('scroll-left');
-    const scrollRightButton = document.getElementById('scroll-right');
+    const scrollLeftButton = document.getElementById('scroll-left2');
+    const scrollRightButton = document.getElementById('scroll-right2');
   
     scrollLeftButton.addEventListener('click', function () {
       cardContainer.scrollBy({ left: -200, behavior: 'smooth' });
@@ -26,8 +44,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
    const cardContainer = document.querySelector('.card-container');
   
-    const scrollLeftButtonCard = document.getElementById('scroll-left2');
-    const scrollRightButtonCard = document.getElementById('scroll-right2');
+    const scrollLeftButtonCard = document.getElementById('scroll-left');
+    const scrollRightButtonCard = document.getElementById('scroll-right');
   
     scrollLeftButtonCard.addEventListener('click', function () {
         picturesContainer.scrollBy({ left: -250, behavior: 'smooth' });
